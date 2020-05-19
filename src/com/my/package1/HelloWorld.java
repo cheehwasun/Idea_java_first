4fda696ee78ff0b644a13786e7ef71a9b3d93d0d
suntux <suntux80@hotmail.com>
2020-05-19 16:48:50 +0800

    git_test02

--- src/com/my/package1/HelloWorld.java
+++ src/com/my/package1/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.my.package1;
-
+//test
 public class HelloWorld {
     public static  void main(String[] args){
-        System.out.print("Hello World");
+        System.out.print("Hello Worldtest");
     }
 }
